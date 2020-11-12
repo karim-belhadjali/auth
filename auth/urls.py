18707ee68745936afaba203781b19155cc1de841
karim-belhadjali <karim.belhadjali1@esprit.tn>
2020-11-12 21:57:05 +0100

    deleted all library

--- auth/urls.py
+++ auth/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authtest.urls')),
-    path('postman/', include('postman.urls'))
 ]
